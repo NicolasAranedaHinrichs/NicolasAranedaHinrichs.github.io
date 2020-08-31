424d9dc32b5c8e4ba4d18148f8c592f4d9ab0601
root <root@debian.example.com>
2020-08-31 14:21:32 -0400

    first commit

---
+++ Scripts/leftside.js
@@ -0,0 +1,35 @@
+document.write('<p> Born in Concepción. Raised in Lindau am Harz <br>\
+      Lives and Works in Leipzig & Hamburg <br>\
+      Biological/Physical Anthropologist <br>\
+      Cognitive Neuroscientist of Language <br>\
+      Writer, Editor and Musician </p>\
+    <table class=dict>\
+      <tr><th>A</th><td> Affrodance <br> Anthropology </td> </tr>\
+      <tr><th>B</th><td> <a href=\"https://open.spotify.com/playlist/09SetQPiVdxtIqAlS4GsNn\">Bähm! Bassline Beauties</a></td></tr>\
+      <tr><th>C</th><td> Cognition </td></tr>\
+      <tr><th>D</th><td> Dostoevsky </td> </tr>\
+      <tr><th>E</th><td> Embodiment <br> Epistemic </td></tr>\
+      <tr><th>F</th></tr>\
+      <tr><th>G</th><td> GO (<a href=\"https://online-go.com/player/477662/\">OGS profile</a>)</td></tr>\
+      <tr><th>H</th><td> Habilitation </td></tr>\
+      <tr><th>I</th></tr>\
+      <tr><th>J</th><td> Jazz </td></tr>\
+      <tr><th>K</th></tr>\
+      <tr><th>L</th><td> Linguistics </td></tr>\
+      <tr><th>M</th><td> Mind <br> Materialism </td></tr>\
+      <tr><th>N</th><td> Neuroscience <br> Negativity </td></tr>\
+      <tr><th>Ñ</th><td> <a href=\"https://www.instagram.com/nicomnom/\">?</a></td></tr>\
+      <tr><th>O</th><td> Ontic <br> Objectification </td></tr>\
+      <tr><th>P</th><td> Perception <br> Positivity </td></tr>\
+      <tr><th>Q</th><td> Queer </td></tr>\
+      <tr><th>R</th><td> Realism </td></tr>\
+      <tr><th>S</th></tr>\
+      <tr><th>T</th><td> Theory is my Praxis <br> Translatology </td></tr>\
+      <tr><th>U</th><td> Ubiquity </td></tr>\
+      <tr><th>V</th></tr>\
+      <tr><th>W</th></tr>\
+      <tr><th>X</th></tr>\
+      <tr><th>Y</th></tr>\
+      <tr><th>Z</th></tr>\
+    </table>\
+');
---
+++ Scripts/navbar.js
@@ -0,0 +1,39 @@
+document.write('<div class="ticker">\
+  <span class=navbar-2>\
+    <a class=home href="index.html"> NICOLAS ARANEDA HINRICHS </a>\
+  </span>\
+  <span class=navbar>\
+     <a class=info href="info.html"> INFO </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=cv href="cv.html"> CV </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=contact href="contact.html"> CONTACT </a>\
+  </span>\
+  <span class=navbar-2>\
+    <a class=home href="index.html"> NICOLAS ARANEDA HINRICHS </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=info  href="info.html"> INFO </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=cv href="cv.html"> CV </a>\
+  </span>\
+  <span class=navbar>\
+   <a class=contact href="contact.html"> CONTACT </a>\
+  </span>\
+  <span class=navbar-2>\
+    <a class=home href="index.html"> NICOLAS ARANEDA HINRICHS </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=info href="info.html"> INFO </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=cv href="cv.html"> CV </a>\
+  </span>\
+  <span class=navbar>\
+    <a class=contact href="contact.html"> CONTACT </a>\
+  </span>\
+</div>\
+');
